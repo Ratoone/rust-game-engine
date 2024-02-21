d97341ea7fb426b33a0bc1f4db3a9d801b688650
Ratoone <vacariu.paul@gmail.com>
2024-02-21 14:05:06 +0100

    feat: Add friction

--- src/engine/systems.rs
+++ src/engine/systems.rs
@@ -1,12 +1,70 @@
 use std::time::Duration;
 
+use ggez::input::keyboard;
 use hecs::{PreparedQuery, World};
 
-use super::components::{Position, Velocity};
+use super::components::{Acceleration, Friction, MovementInput, Position, Velocity};
 
-pub fn system_update_positions(world: &mut World, query: &mut PreparedQuery<(&mut Position, &Velocity)>, delta: &Duration) {
+pub fn system_update_positions(
+    world: &mut World,
+    query: &mut PreparedQuery<(&mut Position, &Velocity)>,
+    delta: &Duration,
+) {
     for (_id, (pos, vel)) in query.query_mut(world) {
         pos.x += vel.dx * delta.as_secs_f32();
         pos.y += vel.dy * delta.as_secs_f32();
+    }
+}
+
+pub fn system_update_velocity(
+    world: &mut World,
+    query: &mut PreparedQuery<(&mut Velocity, &Acceleration)>,
+    delta: &Duration,
+) {
+    for (_id, (vel, acc)) in query.query_mut(world) {
+        vel.dx = (vel.dx + acc.dx * delta.as_secs_f32())
+            .min(acc.max_speed)
+            .max(-acc.max_speed);
+        vel.dy = (vel.dy + acc.dy * delta.as_secs_f32())
+            .min(acc.max_speed)
+            .max(-acc.max_speed);
+    }
+}
+
+pub fn system_friction(
+    world: &mut World,
+    query: &mut PreparedQuery<(&mut Velocity, &Friction)>,
+    delta: &Duration,
+) {
+    for (_id, (vel, friction)) in query.query_mut(world) {
+        vel.dx = if vel.dx == 0.0 {
+            0.0
+        } else {
+            vel.dx.signum()
+                * ((vel.dx.abs() - friction.acceleration * delta.as_secs_f32()).max(0.0))
+        };
+        vel.dy = if vel.dy == 0.0 {
+            0.0
+        } else {
+            vel.dy.signum()
+                * ((vel.dy.abs() - friction.acceleration * delta.as_secs_f32()).max(0.0))
+        };
+    }
+}
+
+pub fn system_player_move(
+    world: &mut World,
+    query: &mut PreparedQuery<(&mut Acceleration, &MovementInput)>,
+    input: ggez::input::keyboard::KeyInput,
+    pressed: bool,
+) {
+    for (_id, (acc, movement)) in query.query_mut(world) {
+        match input.keycode {
+            Some(keyboard::KeyCode::A) => acc.dx = -movement.acceleration * (pressed as i32 as f32),
+            Some(keyboard::KeyCode::D) => acc.dx = movement.acceleration * (pressed as i32 as f32),
+            Some(keyboard::KeyCode::S) => acc.dy = movement.acceleration * (pressed as i32 as f32),
+            Some(keyboard::KeyCode::W) => acc.dy = -movement.acceleration * (pressed as i32 as f32),
+            _ => continue,
+        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,67 @@ pub mod engine;
 
 use std::path;
 
-use engine::{components::{Position, Scale, Velocity}, systems::system_update_positions};
-use ggez::{event, glam::vec2, graphics::{self, Image}, input::keyboard, Context, GameResult};
-use hecs::{Entity, PreparedQuery, World};
+use engine::{
+    components::{Acceleration, Friction, MovementInput, Position, Scale, Velocity},
+    systems::{
+        system_friction, system_player_move, system_update_positions, system_update_velocity,
+    },
+};
+use ggez::{
+    event,
+    glam::vec2,
+    graphics::{self, Image},
+    input::keyboard,
+    Context, GameResult,
+};
+use hecs::{PreparedQuery, World};
 
 struct GameState {
     world: World,
-    player: Entity
 }
 
 impl GameState {
     fn new(ctx: &mut Context) -> GameResult<GameState> {
         let mut world = World::new();
-        let _ = world.spawn_batch(vec![
-            (Position { x: 0.0, y: 0.0 }, Velocity { dx: 0.0, dy: 0.0 }),
-            (Position { x: 200.0, y: 200.0 }, Velocity { dx: 0.0, dy: -1.0 }),
-        ]).collect::<Vec<_>>();
+        let _ = world
+            .spawn_batch(vec![
+                (Position { x: 0.0, y: 0.0 }, Velocity { dx: 0.0, dy: 0.0 }),
+                (
+                    Position { x: 200.0, y: 200.0 },
+                    Velocity { dx: 0.0, dy: -1.0 },
+                ),
+            ])
+            .collect::<Vec<_>>();
 
-        let player = world.spawn((
-            Position { x: 100.0, y: 100.0 }, 
-            Velocity { dx: 0.0, dy: 0.0 }, 
+        world.spawn((
+            Position { x: 100.0, y: 100.0 },
+            Velocity::default(),
+            Acceleration {
+                dx: 0.0,
+                dy: 0.0,
+                max_speed: 50.0,
+            },
             Image::from_path(ctx, "/potion.png")?,
-            Scale { x: 0.1, y: 0.1 }
+            Scale { x: 0.1, y: 0.1 },
+            MovementInput {
+                acceleration: 200.0,
+            },
+            Friction { acceleration: 50.0 },
         ));
 
-        Ok(GameState{world, player})
+        Ok(GameState { world })
     }
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
     fn update(&mut self, ctx: &mut ggez::Context) -> GameResult {
         let mut motion_query = PreparedQuery::<(&mut Position, &Velocity)>::default();
-        system_update_positions(&mut self.world, &mut motion_query,  &ctx.time.delta());
+        let mut acc_query = PreparedQuery::<(&mut Velocity, &Acceleration)>::default();
+        let mut friction_query = PreparedQuery::<(&mut Velocity, &Friction)>::default();
+        let delta = &ctx.time.delta();
+        system_friction(&mut self.world, &mut friction_query, delta);
+        system_update_velocity(&mut self.world, &mut acc_query, delta);
+        system_update_positions(&mut self.world, &mut motion_query, delta);
         Ok(())
     }
 
@@ -42,7 +71,12 @@ impl event::EventHandler<ggez::GameError> for GameState {
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
 
         for (_, (pos, image, scale)) in self.world.query_mut::<(&Position, &Image, &Scale)>() {
-            canvas.draw(image, graphics::DrawParam::new().dest(vec2(pos.x, pos.y)).scale(vec2(scale.x, scale.y)));
+            canvas.draw(
+                image,
+                graphics::DrawParam::new()
+                    .dest(vec2(pos.x, pos.y))
+                    .scale(vec2(scale.x, scale.y)),
+            );
         }
 
         canvas.finish(ctx)?;
@@ -51,28 +85,23 @@ impl event::EventHandler<ggez::GameError> for GameState {
     }
 
     fn key_down_event(
-            &mut self,
-            _ctx: &mut Context,
-            input: ggez::input::keyboard::KeyInput,
-            _repeated: bool,
-        ) -> Result<(), ggez::GameError> {
-        let movement = match input.keycode {
-            Some(keyboard::KeyCode::A) => vec2(-1.0, 0.0),
-            Some(keyboard::KeyCode::D) => vec2(1.0, 0.0),
-            Some(keyboard::KeyCode::S) => vec2(0.0, 1.0),
-            Some(keyboard::KeyCode::W) => vec2(0.0, -1.0),
-            _ => vec2(0.0, 0.0)
-        };
-
-        *self.world.get::<&mut Velocity>(self.player).unwrap() = Velocity {dx: 50.0 * movement.x, dy: 50.0 * movement.y};
-
+        &mut self,
+        _ctx: &mut Context,
+        input: ggez::input::keyboard::KeyInput,
+        _repeated: bool,
+    ) -> Result<(), ggez::GameError> {
+        let mut motion_query = PreparedQuery::<(&mut Acceleration, &MovementInput)>::default();
+        system_player_move(&mut self.world, &mut motion_query, input, true);
         Ok(())
     }
 
-    fn key_up_event(&mut self, _ctx: &mut Context, input: keyboard::KeyInput) -> Result<(), ggez::GameError> {
-        if vec![Some(keyboard::KeyCode::W), Some(keyboard::KeyCode::A), Some(keyboard::KeyCode::S),Some(keyboard::KeyCode::D)].iter().any(|&key| key==input.keycode) {
-            *self.world.get::<&mut Velocity>(self.player).unwrap() = Velocity {dx: 0.0, dy: 0.0 };
-        }
+    fn key_up_event(
+        &mut self,
+        _ctx: &mut Context,
+        input: keyboard::KeyInput,
+    ) -> Result<(), ggez::GameError> {
+        let mut motion_query = PreparedQuery::<(&mut Acceleration, &MovementInput)>::default();
+        system_player_move(&mut self.world, &mut motion_query, input, false);
 
         Ok(())
     }
